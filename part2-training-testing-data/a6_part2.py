046e720bcedaf298d33c2948ed85221e56e444c4
NoahDons <nedons@cps.edu>
2024-12-06 10:56:50 -0600

    part 2 finished

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -14,31 +14,58 @@ y = data["Blood Pressure"].values
 
 # Create your training and testing datasets:
 
+xtrain, xtest, ytrain, ytest = train_test_split(x,y, test_size=.2)
+
 # Use reshape to turn the x values into 2D arrays:
-# xtrain = xtrain.reshape(-1,1)
+x = x.reshape(-1,1)
+xtrain = xtrain.reshape(-1,1)
 
 # Create the model
+model = LinearRegression().fit(xtrain,ytrain)
 
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-
+coefficient = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x,y)
 
 # Print out the linear equation and r squared value:
 
+print("Linear Equation: y=", coefficient, "x +", intercept)
+print("r squared:", r_squared)
 '''
 **********TEST THE MODEL**********
 '''
 # reshape the xtest data into a 2D array
+xtest = xtest.reshape(-1,1)
 
 # get the predicted y values for the xtest values - returns an array of the results
 
+predict = model.predict(xtest)
 # round the value in the np array to 2 decimal places
-
+predict = np.around(predict, 2)
 
 # Test the model by looping through all of the values in the xtest dataset
 print("\nTesting Linear Model with Testing Data:")
-
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index]
+    print("x value:", float(x_coord[0]), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
 '''
 **********CREATE A VISUAL OF THE RESULTS**********
 '''
+
+plt.figure(figsize=(8,6))
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
+plt.title("Blood Pressure by Age")
+
+plt.scatter(xtrain, ytrain, c="blue", label="Training Data")
+plt.scatter(xtest, ytest, c="red", label="Testing Data")
+plt.scatter(xtest, predict, c="orange", label="Predictions")
+
+plt.plot(x, coefficient*x + intercept, c = "green", label = "Line of Best Fit")
+plt.legend()
+plt.show()
--- part2-training-testing-data/part2_writeup.md
+++ part2-training-testing-data/part2_writeup.md
@@ -5,9 +5,11 @@ After completing `a6_part2.py` answer the following questions
 ## Questions to answer
 
 1. What makes this model more effective than the model you created in the previous lesson?
-
+This model is more effective because you can compare the predicted values to the actual values to see if the model is accurate based on real data.
 
 2. What does the R squared coefficient tell you about the model?
-
+The R squared coefficient is quite low at only about .611 which means the model is not very accurate.
 
 3. Would you say that your model is accurate? What evidence supports your conclusion? Consider the meaning of the predicted and actual values in the context of the chart below from the American Heart Association’s website on understanding blood pressure.
+
+The model is not accurate. The differences in blood pressures from the predicted and actual values are off by quite a large margin. For example, my model predicted a person that is 44 years old would have a blood pressure of 137.6 when the actual value was 160. This is a huge margin when dealing with something that needs to be so precise such as blood pressure.
