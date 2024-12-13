22ceadfa34f349eb75780ea22eeba5ef243addb2
NoahDons <nedons@cps.edu>
2024-12-13 13:20:49 -0600

    part 4 done

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -11,17 +11,55 @@ y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
 
+print("X values:", x)
+print("Y values:", y)
 # Step 2: Standardize the data using StandardScaler, 
 
-# Step 3: Transform the data
+scaler = StandardScaler().fit(x)
 
+# Step 3: Transform the data
+x = scaler.transform(x)
 # Step 4: Split the data into training and testing data
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=.2)
 
 # Step 5: Fit the data
 
 # Step 6: Create a LogsiticRegression object and fit the data
+model = linear_model.LogisticRegression().fit(x_train, y_train)
 
 # Step 7: Print the score to see the accuracy of the model
 
+print("Score:", model.score(x_test, y_test))
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+fails = 0
+tests = 0
+print("efoihwwhghew", x_test)
+for index in range(len(x_test)):
+    
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    
+    
+
+    y_pred = int(model.predict(x))
+    if y_pred == 0:
+        y_pred = "Male"
+    else:
+        y_pred = "Female"
+
+    actual = y_test[index]
+    if actual == 0:
+        actual = "Male"
+    else:
+        actual = "Female"
+
+    print("actual:", actual)
+    print("predicted:", y_pred)
+    tests+=1
+    if actual != y_pred:
+        fails+=1
+
+print(fails)
+print(tests)
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,11 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
+The model with the standardscaler is mostly accurate. Out of 80 tests it failed 16 of them which means it is partially accurate but not enough for consistent use.
 
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
-
+The model's accuracy changed slightly with the StandardScaler. Out of 80 tests it failed 14 of them. However this probably isn't due to the scaler but is due to the differing testing and training data. The model is still not accurate enough for consistent use.
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
-
+The model, generally, did well. Though there wasn't perfect accuray the model was mdefinately much better than making complete guesses.
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
-
+Not according to this model. The model said the kind of person who would do this is a male.
